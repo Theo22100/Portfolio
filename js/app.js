f8fbed0527648d0c3677c3a7a85876abcf0da4b9
Théo <theo.guerin35000@gmail.com>
2023-10-15 23:01:31 +0200

    Update app.js
    Ajout carrousel vue js + fix téléphone

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ Vue.component("carousel", {
     data() {
         return {
             currentOffset: 0,
-            windowSize: 3,
+            windowSize: window.innerWidth >= 900 ? 3 : 1,
             paginationFactor: 220,
             items: [
                 {
@@ -119,3 +119,73 @@ var vm5 = new Vue({
         }
     }
 });
+
+var vm6 = new Vue({
+    el: '#app_technolist',
+    data: {
+        technologies: [
+            {
+                name: 'HTML',
+                description: 'Langage de balisage utilisé pour créer la structure et la présentation des pages web.',
+                icon: 'img/icons/html.webp'
+            },
+            {
+                name: 'CSS',
+                description: 'Langage de feuilles de style utilisé pour décrire la présentation d\'un document écrit en HTML.',
+                icon: 'img/icons/css.webp'
+            },
+            {
+                name: 'JavaScript',
+                description: 'Langage de programmation utilisé pour créer des sites web interactifs et dynamiques.',
+                icon: 'img/icons/js.webp'
+            },
+            {
+                name: 'Vue.js',
+                description: 'Cadre JavaScript open-source pour la création d\'interfaces utilisateur.',
+                icon: 'img/icons/vuejs.webp'
+            },
+            {
+                name: 'Scala',
+                description: 'Langage de programmation polyvalent, compatible avec Java, et offrant des fonctionnalités de programmation fonctionnelle et orientée objet.',
+                icon: 'img/icons/scala.webp'
+            },
+            {
+                name: 'GIT',
+                description: 'Système de contrôle de version qui permet aux développeurs de suivre les modifications apportées à leur code, de collaborer avec d\'autres personnes et de revenir facilement aux versions précédentes.',
+                icon: 'img/icons/git.webp'
+            },
+            {
+                name: 'PHP',
+                description: 'Langage de script côté serveur utilisé pour créer des pages web dynamiques.',
+                icon: 'img/icons/php.webp'
+            },
+            {
+                name: 'MySQL',
+                description: 'Système de gestion de base de données relationnelle utilisé pour stocker et récupérer des données.',
+                icon: 'img/icons/mysql.webp'
+            },
+            {
+                name: 'Java',
+                description: 'Langage de programmation polyvalent et orienté objet, fonctionnant sur une machine virtuelle pour une portabilité accrue.',
+                icon: 'img/icons/java.webp'
+            },
+            {
+                name: 'Tailwind',
+                description: 'Framework CSS utilisant des classes utilitaires pour simplifier la création d\'interfaces web élégantes et personnalisables.',
+                icon: 'img/icons/tailwind.webp'
+            }
+        ],
+        leftColumn: [],
+        rightColumn: [],
+    },
+    created() {
+        // Divisez le tableau de technologies en deux colonnes
+        const middleIndex = Math.ceil(this.technologies.length / 2);
+        this.leftColumn = this.technologies.slice(0, middleIndex);
+        this.rightColumn = this.technologies.slice(middleIndex);
+    }
+});
+
+
+
+
